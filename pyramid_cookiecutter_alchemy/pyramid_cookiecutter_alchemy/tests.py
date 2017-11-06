0604bd71a9d2d4075cc1d9055cb300f2bb9206fe
Brian Wheeler <PoundsXD@localhost.localdomain>
2017-11-06 11:09:43 -0500

    Most of step one done.

--- pyramid_cookiecutter_alchemy/pyramid_cookiecutter_alchemy/tests.py
+++ pyramid_cookiecutter_alchemy/pyramid_cookiecutter_alchemy/tests.py
@@ -1,65 +1,21 @@
-import unittest
-import transaction
+"""Test pyramid functions."""
 
-from pyramid import testing
-
-
-def dummy_request(dbsession):
-    return testing.DummyRequest(dbsession=dbsession)
+from pyramid.testing import DummyRequest
+import pytest
 
 
-class BaseTest(unittest.TestCase):
-    def setUp(self):
-        self.config = testing.setUp(settings={
-            'sqlalchemy.url': 'sqlite:///:memory:'
-        })
-        self.config.include('.models')
-        settings = self.config.get_settings()
+@pytest.fixture
+def testsomething():
+    """Create test for something, not sure what."""
+    from webtest import TestApp
+    from pyramid.config import Configurator
 
-        from .models import (
-            get_engine,
-            get_session_factory,
-            get_tm_session,
-            )
+    def main():
+        config = Configurator
+        config.include('pyramid_jinja2')
+        config.include('.routes')
+        config.scan()
+        return config.make_wsgi_app()
 
-        self.engine = get_engine(settings)
-        session_factory = get_session_factory(self.engine)
-
-        self.session = get_tm_session(session_factory, transaction.manager)
-
-    def init_database(self):
-        from .models.meta import Base
-        Base.metadata.create_all(self.engine)
-
-    def tearDown(self):
-        from .models.meta import Base
-
-        testing.tearDown()
-        transaction.abort()
-        Base.metadata.drop_all(self.engine)
-
-
-class TestMyViewSuccessCondition(BaseTest):
-
-    def setUp(self):
-        super(TestMyViewSuccessCondition, self).setUp()
-        self.init_database()
-
-        from .models import MyModel
-
-        model = MyModel(name='one', value=55)
-        self.session.add(model)
-
-    def test_passing_view(self):
-        from .views.default import my_view
-        info = my_view(dummy_request(self.session))
-        self.assertEqual(info['one'].name, 'one')
-        self.assertEqual(info['project'], 'learning_journal')
-
-
-class TestMyViewFailureCondition(BaseTest):
-
-    def test_failing_view(self):
-        from .views.default import my_view
-        info = my_view(dummy_request(self.session))
-        self.assertEqual(info.status_int, 500)
+    something = main()
+    return TestApp(something)
--- pyramid_cookiecutter_alchemy/pyramid_cookiecutter_alchemy/views/default.py
+++ pyramid_cookiecutter_alchemy/pyramid_cookiecutter_alchemy/views/default.py
@@ -1,4 +1,4 @@
-"""."""
+"""Contains functions that serve files."""
 
 from pyramid.response import Response
 import os
@@ -8,26 +8,27 @@ CURRPATH = os.path.dirname(__file__)
 
 
 def list_view(request):
-    """."""
+    """Pull out the index.html."""
     path = os.path.join(CURRPATH, '../templates/index.html')
     with io.open(path) as file:
         return Response(file.read())
 
 
 def detail_view(request):
-    """."""
+    """Pull out the detail.html."""
+    print(request)
     path = os.path.join(CURRPATH, '../templates/detail.html')
     with io.open(path) as file:
         return Response(file.read())
 
 
 def create_view(request):
-    """."""
+    """Pull out the create.html."""
     path = os.path.join(CURRPATH, '../templates/create.html')
     with io.open(path) as file:
         return Response(file.read())
 
 
 def update_view(request):
-    """."""
+    """Pull out the update.html."""
     path = os.path.join(CURRPATH, '../templates/update.html')
