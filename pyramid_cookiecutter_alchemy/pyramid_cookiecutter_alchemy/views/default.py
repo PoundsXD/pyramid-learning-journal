93c95b91951aeccf746983758d26181553a21a2f
Brian Wheeler <PoundsXD@localhost.localdomain>
2017-11-08 18:58:30 -0500

    Fixed a bunch of stuff, really hating psql.

--- pyramid_cookiecutter_alchemy/pyramid_cookiecutter_alchemy/views/default.py
+++ pyramid_cookiecutter_alchemy/pyramid_cookiecutter_alchemy/views/default.py
@@ -2,7 +2,7 @@
 
 from pyramid.view import view_config
 from pyramid.view import view_defaults
-import request
+from ..models.mymodel import Entries
 
 
 @view_defaults(renderer='layout.jinja2')
@@ -14,18 +14,20 @@ class JournalViews:
         self.request = request
 
     @view_config(route_name='home', renderer='../templates/index.jinja2')
-    def list_view(self, rquest):
+    def list_view(self):
         """Serve up the list view."""
-        journals = request.dbsession.query(Journals).all()
+        journals = self.request.dbsession.query(Entries).all()
         return {
             'Journals': journals
         }
 
     @view_config(route_name='detail', renderer='../templates/detail.jinja2')
-    def detail_view(self, request):
+    def detail_view(self):
         """Serve up the detail view."""
+        journal_num = self.request.matchdict['id']
+        journal = self.request.dbsession.query(Entries).get(journal_num)
         return {
-
+            'Journal': journal
         }
 
     @view_config(route_name='create', renderer='../templates/create.jinja2')
